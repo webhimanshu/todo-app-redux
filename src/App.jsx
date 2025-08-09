9834c1b856f34b1707d64dac35927fc8f84efb54
himanshu <himanshusharma2709@gmail.com>
2025-08-09 14:02:04 +0530

    todo app css and redux library added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div class="todo-container">
+      <h1>To-Do List</h1>
+
+      <div class="input-group">
+        <input type="text" id="todo-input" placeholder="Enter a new task..." />
+        <button id="add-btn">Add</button>
+      </div>
+
+      <ul id="todo-list">
+        <li class="todo-item">
+          <input type="checkbox" />
+          <span class="todo-text">Sample Task</span>
+          <div class="action-buttons">
+            <button class="edit-btn">Edit</button>
+            <button class="delete-btn">Delete</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
